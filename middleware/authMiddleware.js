40cc7699cd871cc7de3bf0c8f46b5ea4bfa68ed0
Aditya-2204 <william1binki@gmail.com>
2025-10-05 09:44:08 -0500

    adding middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,16 @@
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
+// lib/authMiddleware.js
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env
 
 function authMiddleware(req, res, next) {
-  const token = req.headers["authorization"]?.split(" ")[1]; // Bearer <token>
-  if (!token) return res.status(401).json({ message: "No token provided" });
+  const token = req.headers['authorization']?.split(' ')[1]; // Bearer <token>
 
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded; // attach user info to request
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
+  if (!token || token !== process.env.BACKEND_TOKEN) {
+    console.log(process.env.BACKEND_TOKEN);
+    return res.status(401).json({ message: 'Unauthorized: Invalid or missing token' } );
   }
+
+  next();
 }
 
 module.exports = authMiddleware;
