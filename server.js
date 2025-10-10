85b2440e9c05d0985668c27579c7016223eb0cd1
Brainspark1 <165429616+Brainspark1@users.noreply.github.com>
2025-10-10 18:52:12 -0500

    implemented approval emails with nodemailer in backend, and enabled admin to set status of users

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import submissionRoutes from './routes/submissions.js';
+
 const express = require('express');
 const app = express();
 require('./db/db'); // MongoDB connection
@@ -8,8 +10,12 @@ const userRoutes = require('./routes/users');
 app.use('/', require('./routes/index'));
 app.use('/users', userRoutes);
 
+mongoose.connect(process.env.MONGODB_URI);
+
 // Listen on port 0 (random free port)
 const server = app.listen(8000, () => {
   const port = server.address().port; // get actual port
   console.log(`Server running on http://localhost:${port}`);
 });
+
+app.use('/api/submissions', submissionRoutes);
