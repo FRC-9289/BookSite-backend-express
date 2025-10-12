fa307725dd28f25f9fc6ce094ff7dcf165561422
Aditya-2204 <william1binki@gmail.com>
2025-10-11 20:20:16 -0500

    fixing logic

--- routes/Router.js
+++ routes/Router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { postStudent, getStudentByGrade, getRoom, getRoomsSubmissions, manageStatus} from "../controllers/Student/controller.js";
+import { postStudent, getStudentByGrade, getRoom, getRoomsSubmissions, manageStatus, getSubmissions} from "../controllers/Student/controller.js";
 
 const router = Router();
 
@@ -14,7 +14,7 @@ router.post("/student-post", authMiddleware, upload.fields([
   { name: "file3", maxCount: 1 }
 ]), postStudent);
 
-router.get("/submissions", authMiddleware, getStudentByGrade);
+router.get("/submissions", authMiddleware, getSubmissions);
 
 router.get("/room-get", authMiddleware, getRoom);
 router.get("/rooms-get", authMiddleware, getRoomsSubmissions);
